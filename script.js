138720d81319b5b4b4bd5a58147e5c4f4ca99100
Ricardo Alves <ralvespiedade@gmail.com>
2022-03-10 12:43:21 -0300

    adjusting frontend style

--- script.js
+++ script.js
@@ -4,17 +4,17 @@ var counter = 0
 
 for (counter = 0; counter <= 7; counter++) {
     var item = document.createElement("li")
-
+    item.classList.add('product')
     item.innerHTML = `
-        <li class="product">
-            <img class="productImage" src="testImage.jpg" alt="product image">
-            <div class="productName">Nome do Produto</div>
-            <p class="productDescription">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima possimus ea ex saepe sequi </p>
-            <div class="productOldValue">De R$ 100,00</div>
-            <div class="productCurrentValue">Por R$ 90,00</div>
-            <div class="productPaymentOption">Ou em 2x no cartão</div>
-            <button class="buyProductButton">Comprar</button>
-        </li>
+        
+        <img class="productImage" src="testImage.jpg" alt="product image">
+        <div class="productName">Nome do produto</div>
+        <p class="productDescription">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima possimus ea ex saepe sequi. </p>
+        <div class="productOldValue">De: R$100,00</div>
+        <div class="productCurrentValue">Por: R$90,00</div>
+        <div class="productPaymentOption">Ou 2x de R$9,99</div>
+        <button class="buyProductButton">Comprar</button>
+        
     `
     productList.appendChild(item)
 
